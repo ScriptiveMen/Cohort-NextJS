4b604dd0f416a75ecd4f15c1c9f2bb9164e59eba
ScriptiveMen <i.satyakumarram@gmail.com>
2025-09-13 22:07:26 +0530

    Wrapped Up Next.Js - Image Optimization

--- class1/app/page.js
+++ class1/app/page.js
@@ -1,9 +1,13 @@
+import Image from "next/image";
 import React from "react";
+import spoon from "../public/spoon.avif";
 
 const page = () => {
   return (
     <div>
       <h1>Home Page</h1>
+
+      <Image height={300} alt="spoon" src={spoon} />
     </div>
   );
 };
