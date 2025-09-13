77748bb6c00715da40b5c4cfa4ed396d6d822571
ScriptiveMen <i.satyakumarram@gmail.com>
2025-09-13 20:34:17 +0530

    NotFound route implemented

---
+++ class1/app/not-found.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <div>
+      <h1 className="text-red-400">
+        Requested Page is <br /> not available.
+      </h1>
+    </div>
+  );
+};
+
+export default NotFound;
